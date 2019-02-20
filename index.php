e8ed4bb88414daab80e1e89136703b27e2a8fc69
roby8martin <roby8martin@gmail.com>
2019-02-20 10:22:39 +0100

    creato il form di partenza

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="it">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -16,8 +16,27 @@
   </head>
 
   <body>
-    <h1>Hello, world!</h1>
 
+	  <div class="container">
+	    <form>
+	  		<div class="form-group">
+	    		<label for="nome">Nome</label>
+	    		<input type="text" class="form-control" id="nome" placeholder="Inserisci il tuo nome">
+	  		</div>
+
+	  		<div class="form-group">
+	    		<label for="cognome">Cognome</label>
+	    		<input type="text" class="form-control" id="cognome" placeholder="Inserisci il tuo cognome">
+	  		</div>
+		  
+			  <div class="form-group">
+			    <label for="indirizzo">Indirizzo</label>
+			    <textarea class="form-control" id="indirizzo" rows="3"></textarea>
+			  </div>
+
+			  <button type="submit" class="btn btn-primary">Invia</button>
+			</form>
+		</div>
     
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
